d1ce36c0ff21a1abf7059b540176cdd1b6ab361b
Arjun Bharti <60930192+arjunbharti@users.noreply.github.com>
2020-03-28 20:21:08 +0530

    Add files via upload

---
+++ lib/screen/Status.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../reuseable/Wcards.dart';
+import '../model/chat.dart';
+
+
+import 'package:flutter/painting.dart';class Status extends StatefulWidget {
+  @override
+  _StatusState createState() => _StatusState();
+}
+
+class _StatusState extends State<Status> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Wcards("My Status", "https://images.pexels.com/photos/1149022/pexels-photo-1149022.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            "Tab to add status"),
+        Divider(),
+        Heading("Resent updates"),
+        Wcards(messageData[2].name, messageData[2].imageUrl, messageData[2].time),
+        Wcards(messageData[3].name, messageData[3].imageUrl, messageData[2].time),
+      ],
+    );
+  }
+}
+
+
+class Heading extends StatelessWidget {
+
+  final String heading;
+  Heading(this.heading);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.only(left: 5.0, top: 5.0),
+        child: Text(
+          heading,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+}
